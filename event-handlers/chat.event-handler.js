bb04f663e987e7105460681f16ee02b3498c7bf8
Hyllesen <stefanhyltoft@gmail.com>
2019-09-16 12:55:52 +0200

    PM unit test pass

--- event-handlers/chat.event-handler.js
+++ event-handlers/chat.event-handler.js
@@ -1,4 +1,5 @@
-const { CHAT_MESSAGE } = require("../eventTypes");
+const _ = require("lodash");
+const { CHAT_MESSAGE, PRIVATE_MESSAGE } = require("../eventTypes");
 
 function handleChat(io, socket) {
   socket.on(CHAT_MESSAGE, msg => {
@@ -6,4 +7,12 @@ function handleChat(io, socket) {
   });
 }
 
-module.exports = { handleChat };
+function handlePrivateChat(io, socket, people) {
+  socket.on(PRIVATE_MESSAGE, msg => {
+    const sockets = _.invert(people);
+    const socketIdToMessage = sockets[msg.to];
+    io.to(socketIdToMessage).emit(PRIVATE_MESSAGE, msg);
+  });
+}
+
+module.exports = { handleChat, handlePrivateChat };
--- tests/unit-test/chat.unit.test.js
+++ tests/unit-test/chat.unit.test.js
@@ -1,11 +1,23 @@
 const chatHandler = require("../../event-handlers").chatHandler;
-const { CHAT_MESSAGE } = require("../../eventTypes");
+const { CHAT_MESSAGE, PRIVATE_MESSAGE } = require("../../eventTypes");
 const MockedSocket = require("socket.io-mock");
 
 const socket = new MockedSocket();
+const client2 = new MockedSocket();
 const io = new MockedSocket();
 
 jest.spyOn(io, "emit");
+
+const emitMock = jest.fn();
+
+let ioWithTo = {
+  to: jest.fn(() => ({ emit: emitMock }))
+};
+
+people = {
+  fdmoidmfdim: "ClientUser1",
+  fidoimfds9: "ClientUser2"
+};
 
 describe("ChatHandler", () => {
   it("Send chat message to all users", () => {
@@ -13,5 +25,19 @@ describe("ChatHandler", () => {
     chatHandler.handleChat(io, socket);
     socket.socketClient.emit(CHAT_MESSAGE, testMessage);
     expect(io.emit).toHaveBeenCalledWith(CHAT_MESSAGE, testMessage);
+  });
+
+  it("should be able to send private message to another user", () => {
+    chatHandler.handlePrivateChat(ioWithTo, socket, people);
+    chatHandler.handlePrivateChat(ioWithTo, client2, people);
+    socket.socketClient.emit(PRIVATE_MESSAGE, {
+      to: "ClientUser2",
+      message: "Hi Client2!"
+    });
+    expect(emitMock).toHaveBeenCalledWith(PRIVATE_MESSAGE, {
+      to: "ClientUser2",
+      message: "Hi Client2!"
+    });
+    expect(ioWithTo.to).toHaveBeenCalledWith("fidoimfds9");
   });
 });
